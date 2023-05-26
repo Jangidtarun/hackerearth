0fec3fc173c9b8c7008b7f7a82ed8ce2f513eaa2
tarun jangid <tarunjangid19102002@gmail.com>
2023-05-26 14:30:02 +0530

    ranji

---
+++ unlock-the-door.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int M = 1e9 + 7;
+
+int binexp(int a, int b, int m)
+{
+    int ans = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            ans = (ans * 1LL * a) % m;
+        a = (a * 1LL * a) % m;
+        b >>= 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    const int N = 1e5+10;
+    int fact[N];
+    fact[0] = 1;
+    for (int i = 1; i < N; i++)
+        fact[i] = (fact[i - 1] * 1LL * i) % M;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int ans = fact[n];
+        ans = (ans * 1LL * fact[k]) % M;
+        int den = (fact[n] * 1LL * fact[k - n]) % M;
+        ans = (ans * 1LL * binexp(den, M - 2, M)) % M;
+        cout << ans << endl;
+    }
+}
